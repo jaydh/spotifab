34f5154de2140eb883ebb4d480e8fa854b99eaf7
Jay Howard <jayhoward300@gmail.com>
2018-07-11 13:19:55 -0700

    Downgrade electron to use widevinecdm for windows compatibility

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -3,34 +3,42 @@ const url = require('url');
 const path = require('path');
 const widevine = require('electron-widevinecdm');
 const isDev = require('electron-is-dev');
+const windowStateKeeper = require('electron-window-state');
 widevine.load(app);
 let win;
 
 function createWindow() {
-  // Create the browser window.
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 400
+  });
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
     webPreferences: {
       plugins: true
     }
   });
-
-  win.loadURL(
-    isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
+  mainWindowState.manage(win);
+  // Create the browser window.
+  win.loadURL('https://spotifab-3379e.firebaseapp.com/');
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+  globalShortcut.register('MediaNextTrack', () =>
+    win.webContents.executeJavaScript(`window.nextTrack()`)
   );
-  win.webContents.openDevTools();
-  globalShortcut.register('CommandOrControl+j', () => {
-    win.webContents.executeJavaScript(`window.nextTrack()`);
-  });
-  globalShortcut.register('CommandOrControl+k', () => {
-    win.webContents.executeJavaScript(`window.previousTrack()`);
-  });
-  globalShortcut.register('CommandOrControl+p', () => {
-    win.webContents.executeJavaScript(`window.togglePlay()`);
-  });
+  globalShortcut.register('MediaPreviousTrack', () =>
+    win.webContents.executeJavaScript(`window.previousTrack()`)
+  );
+  globalShortcut.register('MediaPlayPause', () =>
+    win.webContents.executeJavaScript(`window.togglePlay()`)
+  );
+  globalShortcut.register('VolumeMute', () =>
+    win.webContents.executeJavaScript(`window.toggleMute()`)
+  );
 
   win.on('closed', () => {
     win = null;
