8cb05628922db42416e62faa825373519fbba862
Jay Howard <jayhoward300@gmail.com>
2018-07-13 21:31:38 -0700

    Move config to apiKeys file

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import { firebaseConf } from './apiKeys';
+
 import * as firebase from 'firebase';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
@@ -11,30 +13,23 @@ import { nextSong, prevSong, togglePlay } from './actions/songActions';
 import { toggleMute } from './actions/soundActions';
 import App from './App';
 import reducers from './reducers';
-const config = {
-  apiKey: 'AIzaSyBiqbvG93Vd0tqHkNGZNg6VBHUC4onS3jE',
-  authDomain: 'spotifab-3379e.firebaseapp.com',
-  databaseURL: 'https://spotifab-3379e.firebaseio.com',
-  projectId: 'spotifab-3379e',
-  storageBucket: '',
-  messagingSenderId: '1045511818191'
-};
-const app = firebase.initializeApp(config);
-console.log(app);
 declare module 'redux' {
   export type GenericStoreEnhancer = any;
 }
 
+const app = firebase.initializeApp(firebaseConf);
+export const database = app.firestore();
 export const store = createStore(
   reducers,
   composeWithDevTools(applyMiddleware(thunk))
 );
+const persistor = persistStore(store);
+
 (window as any).nextTrack = () => store.dispatch<any>(nextSong());
 (window as any).previousTrack = () => store.dispatch<any>(prevSong());
 (window as any).togglePlay = () => store.dispatch<any>(togglePlay());
 (window as any).volumeMute = () => store.dispatch<any>(toggleMute());
 
-const persistor = persistStore(store);
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
