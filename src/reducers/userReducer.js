57454cc21366e391996677a7982ae9f1dd992428
Jay Howard <jayhoward300@gmail.com>
2018-07-18 02:35:56 -0700

    Add firebase state to user reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,37 +1,43 @@
-export const userReducer = (state = {}, action) => {
-
+export const userReducer = (
+  state = {
+    signedIn: false
+  },
+  action
+) => {
   switch (action.type) {
+    case 'FETCH_USER_SUCCESS':
+      return {
+        ...state,
+        user: action.user,
+        fetchUserError: false
+      };
 
-  case "FETCH_USER_SUCCESS":
-    return {
-      ...state,
-      user: action.user,
-      fetchUserError: false
-    };
+    case 'FETCH_USER_ERROR':
+      return {
+        ...state,
+        fetchUserError: true
+      };
 
-  case "FETCH_USER_ERROR":
-    return {
-      ...state,
-      fetchUserError: true
-    };
+    case 'ADD_SONG_TO_LIBRARY_SUCCESS':
+      return {
+        ...state,
+        songAddedToLibrary: true,
+        songId: action.songId
+      };
 
-  case "ADD_SONG_TO_LIBRARY_SUCCESS":
-    return {
-      ...state,
-      songAddedToLibrary: true,
-      songId: action.songId
-    };
+    case 'ADD_SONG_TO_LIBRARY_ERROR':
+      return {
+        ...state,
+        songAddedToLibrary: false
+      };
+    case 'SIGN_IN':
+      return { ...state, signedIn: true, firebaseUser: action.user };
+    case 'SIGN_OUT':
+      return { ...state, signedIn: false, firebaseUser: null };
 
-  case "ADD_SONG_TO_LIBRARY_ERROR":
-    return {
-      ...state,
-      songAddedToLibrary: false
-    };
-
-  default:
-    return state;
+    default:
+      return state;
   }
-
 };
 
 export default userReducer;
