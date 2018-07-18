725bd14833c2f59f4c43f0772e4d1f1dafc74f37
Jay Howard <jayhoward300@gmail.com>
2018-07-18 02:27:41 -0700

    Remove unused components

--- src/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import SongProgress from '../SongProgress';
-import './Footer.css';
-
-const Footer = () => {
-  return <SongProgress />;
-};
-export default Footer;
