868bfca54c8a0410c9df6d543fca83ca67ee6bd8
Jay Howard <jayhoward300@gmail.com>
2018-08-05 22:26:43 -0700

    Add token retrieval action to on firebase login

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-import { firebaseConf } from './apiKeys';
-
+import { addMinutes, isBefore, parse } from 'date-fns';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
@@ -7,6 +6,7 @@ import * as firebaseui from 'firebaseui';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
@@ -14,8 +14,11 @@ import { PersistGate } from 'redux-persist/integration/react';
 import thunk from 'redux-thunk';
 import { nextSong, prevSong, togglePlay } from './actions/songActions';
 import { toggleMute } from './actions/soundActions';
+import { listenForToken, requestTokenRefresh } from './actions/tokenActions';
+import { firebaseConf } from './apiKeys';
 import App from './App';
 import reducers from './reducers';
+
 declare module 'redux' {
   export type GenericStoreEnhancer = any;
 }
@@ -24,45 +27,38 @@ export const app = firebase.initializeApp(firebaseConf);
 export const database = app.firestore();
 const settings = { timestampsInSnapshots: true };
 database.settings(settings);
+export const ui = new firebaseui.auth.AuthUI(app.auth());
+
 export const store = createStore(
   reducers,
   composeWithDevTools(applyMiddleware(thunk))
 );
 const persistor = persistStore(store);
-
 (window as any).nextTrack = () => store.dispatch<any>(nextSong());
 (window as any).previousTrack = () => store.dispatch<any>(prevSong());
 (window as any).togglePlay = () => store.dispatch<any>(togglePlay());
 (window as any).volumeMute = () => store.dispatch<any>(toggleMute());
+
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
     store.dispatch({ type: 'SIGN_IN', user });
+    store.dispatch<any>(listenForToken());
+    const { token, time } = store.getState().token;
+    if (token && !isBefore(new Date(), addMinutes(parse(time), 30))) {
+      store.dispatch<any>(requestTokenRefresh());
+    }
   } else {
     store.dispatch({ type: 'SIGN_OUT' });
   }
 });
-const ui = new firebaseui.auth.AuthUI(app.auth());
-const uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-      return false;
-    },
-    uiShown: () => {
-      document.getElementById('loader')!.style.display = 'none';
-    },
-
-    signInSuccessUrl: 'https://spotifab-3379e.firebaseapp.com/'
-  },
-  signInFlow: 'redirect',
-  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID]
-};
-ui.start('#firebaseui-auth-container', uiConfig);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
