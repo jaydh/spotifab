7a69dd5880cf2b725b95d59829a953122e0b4da7
Jay Howard <jayhoward300@gmail.com>
2018-08-05 22:27:03 -0700

    Add action to create new playlists

--- src/actions/playlistActions.js
+++ src/actions/playlistActions.js
@@ -1,3 +1,5 @@
+import { List } from 'immutable';
+
 export const fetchPlaylistMenuPending = () => {
   return {
     type: 'FETCH_PLAYLIST_MENU_PENDING'
@@ -90,5 +92,66 @@ export const fetchPlaylistSongs = (userId, playlistId) => {
       .catch(err => {
         dispatch(fetchPlaylistSongsError(err));
       });
+  };
+};
+
+export const createNewPlaylist = (name, description) => {
+  return async (dispatch, getState) => {
+    const accessToken = getState().token.token;
+    const userId = getState().userReducer.user.id;
+    const trackURIs = getState().queue.queue.map(t => t.track.uri);
+    await fetch(`https://api.spotify.com/v1/users/${userId}/playlists/`, {
+      method: 'POST',
+      body: JSON.stringify({
+        name,
+        description,
+        public: true
+      }),
+      headers: new Headers({
+        Authorization: 'Bearer ' + accessToken
+      })
+    });
+    const playlists = (await (await fetch(
+      'https://api.spotify.com/v1/me/playlists',
+      {
+        headers: new Headers({
+          Authorization: 'Bearer ' + accessToken
+        })
+      }
+    )).json()).items;
+    const newPlaylist = List(playlists).filter(t => t.name === name);
+    await fetch(
+      `https://api.spotify.com/v1/users/${userId}/playlists/${
+        newPlaylist.get(0).id
+      }/tracks`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          uris: trackURIs
+        }),
+        headers: new Headers({
+          Authorization: 'Bearer ' + accessToken
+        })
+      }
+    );
+    dispatch(fetchPlaylistsMenu());
+  };
+};
+
+export const unfollowPlaylist = id => {
+  return async (dispatch, getState) => {
+    const accessToken = getState().token.token;
+    const userId = getState().userReducer.user.id;
+    console.log('ehy');
+    await fetch(
+      `https://api.spotify.com/v1/users/${userId}/playlists/${id}/followers`,
+      {
+        method: 'DELETE',
+        headers: new Headers({
+          Authorization: 'Bearer ' + accessToken
+        })
+      }
+    );
+    dispatch(fetchPlaylistsMenu());
   };
 };
