cdac705ec578d1ed31e3d590ade24c1a0d2ef5b7
Jay Howard <jayhoward300@gmail.com>
2018-08-05 22:36:48 -0700

    Add immutable lists for playlist reducer

--- src/reducers/playlistReducer.js
+++ src/reducers/playlistReducer.js
@@ -1,39 +1,36 @@
+import { List } from 'immutable';
 export const playlistReducer = (state = {}, action) => {
   switch (action.type) {
+    case 'FETCH_PLAYLIST_MENU_PENDING':
+      return {
+        ...state,
+        fetchPlaylistPending: true
+      };
 
-  case "FETCH_PLAYLIST_MENU_PENDING":
-    return {
-      fetchPlaylistPending: true,
-      ...state
-    };
+    case 'FETCH_PLAYLIST_MENU_SUCCESS':
+      return {
+        ...state,
+        playlistMenu: List(action.playlists),
+        playlists: List(action.playlists),
+        fetchPlaylistError: false,
+        fetchPlaylistPending: false
+      };
 
-  case "FETCH_PLAYLIST_MENU_SUCCESS":
-    return {
-      playlistMenu: action.playlists,
-      playlists: action.playlists,
-      fetchPlaylistError: false,
-      fetchPlaylistPending: false,
-      ...state
-    };
+    case 'ADD_PLAYLIST_ITEM':
+      return {
+        ...state,
+        playlists: [...state.playlists, action.playlist]
+      };
 
-  case "ADD_PLAYLIST_ITEM":
-    return {
-      ...state,
-      playlists: [
-        ...state.playlists,
-        action.playlist
-      ]
-    };
+    case 'FETCH_PLAYLIST_MENU_ERROR':
+      return {
+        fetchPlaylistError: true,
+        fetchPlaylistPending: false,
+        ...state
+      };
 
-  case "FETCH_PLAYLIST_MENU_ERROR":
-    return {
-      fetchPlaylistError: true,
-      fetchPlaylistPending: false,
-      ...state
-    };
-
-  default:
-    return state;
+    default:
+      return state;
   }
 };
 
