dfc801faf8d4c6a4d9d3fe6eea7695774fa0f6fc
Jay Howard <jayhoward300@gmail.com>
2018-08-23 01:10:04 -0700

    Add async components

--- src/components/MainView/component.js
+++ src/components/MainView/component.js
@@ -1,66 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import SongList from '../SongList';
-import AlbumList from '../AlbumList';
-import ArtistList from '../ArtistList';
-import BrowseView from '../BrowseView';
-import Queue from '../Queue';
-import SongProgress from '../SongProgress';
-import Authenticate from '../Authenticate';
 import { Route, Switch } from 'react-router';
-import initAPIs from '../../initSpotifySDK';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { fetchSongs, fetchYoutubeSongs } from '../../actions/songActions';
-
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators(
-    {
-      fetchSongs,
-      fetchYoutubeSongs
-    },
-    dispatch
-  );
-};
-const SongMainConnect = connect(
-  null,
-  mapDispatchToProps
-)(
-  class SongMain extends React.Component {
-    constructor() {
-      super();
-      initAPIs();
-    }
-    componentDidMount() {
-      if (this.props.location.pathname.startsWith('/library')) {
-        this.props.fetchSongs();
-        this.props.fetchYoutubeSongs();
-      }
-    }
-    componentWillReceiveProps(nextProps) {
-      console.log(nextProps.location.pathname);
-      if (nextProps.location.pathname.startsWith('/library')) {
-        this.props.fetchSongs();
-        this.props.fetchYoutubeSongs();
-      }
-    }
-    render() {
-      return (
-        <div className="main-view">
-          <SongList />
-          <Queue />
-          <SongProgress />
-        </div>
-      );
-    }
-  }
-);
+import asyncComponent from '../AsyncComponent';
+const SongMain = asyncComponent(() => import('../SongMain'));
+const SongList = asyncComponent(() => import('../SongList'));
+const Queue = asyncComponent(() => import('../Queue'));
+const Authenticate = asyncComponent(() => import('../Authenticate'));
+const SongProgress = asyncComponent(() => import('../SongProgress'));
 
 const Downloads = () => (
-  <div>
-    <a href="https://firebasestorage.googleapis.com/v0/b/spotifab-3379e.appspot.com/o/spotilyrics%200.1.0.exe?alt=media&token=6db41137-2479-4eec-9a0f-fd3766c56545">
-      Windows Desktop App
-    </a>
+  <div
+    style={{
+      display: 'flex',
+      margin: '0 auto',
+      alignContent: 'center',
+      justifyContent: 'center'
+    }}
+  >
+    <button className="btn">
+      <a href="https://firebasestorage.googleapis.com/v0/b/spotifab-3379e.appspot.com/o/bard-1.0.0-mac.zip?alt=media&token=b8c20b23-94d6-4ab0-bf5e-7a129c4ed123">
+        Mac App
+      </a>{' '}
+    </button>
+    <button className="btn">
+      {' '}
+      <a href="https://firebasestorage.googleapis.com/v0/b/spotifab-3379e.appspot.com/o/bard%20Setup%201.0.0.exe?alt=media&token=0abd1149-47f3-494c-ab16-71b135aada96">
+        Windows Desktop App
+      </a>
+    </button>
+    <button className="btn">
+      <a href="https://firebasestorage.googleapis.com/v0/b/spotifab-3379e.appspot.com/o/bard-1.0.0-x86_64.AppImage?alt=media&token=2106fa05-a39e-47e1-a3b0-914131c616ed">
+        Linux AppImage{' '}
+      </a>{' '}
+    </button>
   </div>
 );
 
@@ -69,9 +41,10 @@ class MainView extends React.Component {
     return (
       <Switch>
         <Route exact path="/" component={Authenticate} />
-        <Route path="/library" component={SongMainConnect} />
-        <Route path="/playlist/" component={SongMainConnect} />
+        <Route path="/library" component={SongMain} />
+        <Route path="/playlist/" component={SongMain} />
         <Route path="/authenticate" component={Authenticate} />
+        <Route path="/authenticate/:auth" component={Authenticate} />
         <Route path="/downloads" component={Downloads} />
       </Switch>
     );
