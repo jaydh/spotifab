58716462c4b71284fc8eb44fb28bf699af979164
Jay Howard <jayhoward300@gmail.com>
2018-09-07 01:22:14 -0700

    Add debounced

--- src/actions/soundActions.js
+++ src/actions/soundActions.js
@@ -1,18 +1,32 @@
 export const updateVolume = volume => {
-  window.player.setVolume(volume);
-  window.ytPlayer.setVolume(volume * 100);
-  return {
-    type: 'UPDATE_VOLUME',
-    volume
+  return async dispatch => {
+    const thunk = window.player
+      .setVolume(volume)
+      .then(() => window.ytPlayer.setVolume(volume * 100))
+      .then(() =>
+        dispatch({
+          type: 'UPDATE_VOLUME',
+          volume
+        })
+      );
   };
+  thunk.meta = {
+    debounce: {
+      time: '500',
+      key: Math.random()
+    }
+  };
+  return thunk;
 };
 
 export const toggleMute = () => {
-  return (dispatch, getState) => {
+  return async (dispatch, getState) => {
     const muted = getState().player.muted;
     const volume = getState().player.volume;
-    muted ? window.player.setVolume(volume) : window.player.setVolume(0);
-    muted
+    (await muted)
+      ? window.player.setVolume(volume)
+      : window.player.setVolume(0);
+    (await muted)
       ? window.ytPlayer.setVolume(volume * 100)
       : window.ytPlayer.setVolume(0);
     dispatch({
